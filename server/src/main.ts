599fcce7660afa5b20dd1aafb6e004d334734e09
Hung Vo Viet <vohungbk@gmail.com>
2024-01-28 13:55:30 +0700

    Update command start app

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(3000);
 }
 bootstrap();
